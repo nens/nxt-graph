78467524396aa764accb6e23e898784d718b9876
Jack Ha <jackha@gmail.com>
2013-07-17 16:08:07 +0200

    readme and setup.py

--- setup.py
+++ setup.py
@@ -23,15 +23,15 @@ tests_require = [
 
 setup(name='nxt-graph',
       version=version,
-      description="TODO",
+      description="Lizard 5 graph library using D3.js",
       long_description=long_description,
       # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=['Programming Language :: Python',
                    'Framework :: Django',
                    ],
       keywords=[],
-      author='TODO',
-      author_email='TODO@nelen-schuurmans.nl',
+      author='Jack Ha',
+      author_email='jack.ha@nelen-schuurmans.nl',
       url='',
       license='GPL',
       packages=['nxt_graph'],
